a72cb2ba9112d9a708b980760f76c366925c4307
Karl <marchikarl1@gmail.com>
2023-10-07 20:17:21 +0200

    Some updates

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify, request
 from hashlib import sha1, sha256, sha512, md5
+from urllib.parse import unquote
+from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
+
+CORS(app)
 
 hash_types = {
     "SHA256": sha256,
@@ -10,14 +15,36 @@ hash_types = {
     "MD5": md5
 }
 
+db = sqlite3.connect("database.db")
+cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS match (password TEXT, hash TEXT, salt TEXT);")
+db.commit()
 
+def get_from_db(hash_str: str, salt: str = None):
+    db = sqlite3.connect("database.db")
+    cur = db.cursor()
+    if salt:
+        fetch = cur.execute("SELECT password FROM match WHERE hash = ? AND salt = ?", (hash_str, salt))
+    else:
+        fetch = cur.execute("SELECT password FROM match WHERE hash = ?", (hash_str,))
+    return fetch.fetchone()
 
 def decrypt(data: dict):
+    db = sqlite3.connect("database.db")
+    cur = db.cursor()
     hash_str = data.get("hash")
     salt = data.get("salt")
     hash_type = data.get("type")
     if hash_type not in hash_types or hash_str is None:
         return {"message": "Invalid decrypt"}
+    hash_str = unquote(data.get("hash"))
+    hash_type = unquote(data.get("type"))
+
+    if salt is not None:
+        salt = unquote(data.get("salt"))
+
+    if data_in_db := get_from_db(hash_str, salt):
+        return {"password": data_in_db[0]}
 
     hasher = hash_types[hash_type]
 
@@ -26,32 +53,38 @@ def decrypt(data: dict):
             line = line.strip()
             if salt:
                 if hasher(line.encode() + salt.encode()).hexdigest() == hash_str or \
-                hasher(hasher(line.encode()).hexdigest().encode() + salt.encode()).hexdigest() == hash_str:
+                hasher(hasher(line.encode()).hexdigest().encode() + salt.encode()).hexdigest() == hash_str or \
+                hasher(hasher(salt.encode()).hexdigest().encode() + line.encode()).hexdigest() == hash_str:
+                    cur.execute("INSERT INTO match (password, hash, salt) VALUES (?, ?, ?);", (line, hash_str, salt))
+                    db.commit()
                     return {"password": line}
             elif hasher(line.encode()).hexdigest() == hash_str:
+                cur.execute("INSERT INTO match (password, hash) VALUES (?, ?);", (line, hash_str))
+                db.commit()
                 return {"password": line}
 
         return {"message": "The password could not be found in rockyou"}
-
-
-
 
 @app.route('/decrypt')
 def decrypt_route():
     args = request.args
     data = {}
 
-    if "hash" in args:
+    if "hash" in args and (args["hash"].strip() != ''):
         data["hash"] = args["hash"]
-    if "salt" in args:
+    else:
+        return jsonify({
+            "message": "Please set a hash !"
+        })
+    if "salt" in args and (args["salt"].strip() != ''):
         data["salt"] = args["salt"]
-    if "type" in args:
+    if "type" in args and (args["type"].strip() != ''):
         data["type"] = args["type"]
     else:
+        print(args)
         return jsonify({
             "message": "Please set a hash type !"
         })
-    
 
     return jsonify(decrypt(data))
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,38 +7,62 @@ export default function Home() {
     const [hash, setHash] = useState('');
     const [salt, setSalt] = useState('');
     const [type, setType] = useState('');
+    const [message, setMessage] = useState('');
+    const [messageClass, setMessageClass] = useState('');
+    const [loading, setLoading] = useState(false);
     const makeRequest = () => {
+        setMessage('');
+        setLoading(true);
         const asynMake = async () => {
-            const req = await fetch(`http://127.0.0.1/decrypt?hash=${hash}&`)
+            const req = await fetch(`http://127.0.0.1/decrypt?hash=${hash}&salt=${salt}&type=${type}`)
+            const json = await req.json()
+            if ("message" in json) {
+                setLoading(false);
+                setMessageClass('text-center text-red-600 font-bold mt-5');
+                setMessage(json["message"]);
+            } else if ("password" in json) {
+                setLoading(false);
+                setMessageClass('text-center text-green-600 font-bold mt-5')
+                setMessage(`Password: ${json["password"]}`)
+
+            }
         }
+        asynMake();
     }
     return (
         <main>
             <div className='bg-gray-600 w-1/2 h-auto m-auto mt-20 pt-20 rounded-xl pb-20'>
                 <div>
-                    <label htmlFor="hash" className="text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hash<span className="text-red-600">*</span></label>
-                    <input onChange={e => setHash(e.currentTarget.value)} type="text" id="first_name" className="mb-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-auto" placeholder="Your hash" required />
+                    <label htmlFor="hash" className="text-orange-400 text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hash<span className="text-red-600">*</span></label>
+                    <input onChange={e => setHash(encodeURIComponent(e.currentTarget.value))} type="text" id="first_name" className="mb-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-auto" placeholder="Your hash" required />
                 </div>
                 <div>
-                    <label htmlFor="salt" className="text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">Salt</label>
-                    <input onChange={e => setSalt(e.currentTarget.value)} type="text" id="first_name" className="mb-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-auto" placeholder="Your salt" />
+                    <label htmlFor="salt" className="text-yellow-200 text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">Salt</label>
+                    <input onChange={e => setSalt(encodeURIComponent(e.currentTarget.value))} type="text" id="first_name" className="mb-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 m-auto" placeholder="Your salt" />
                 </div>
                 <label htmlFor="underline_select" className="sr-only">Underline select</label>
-                <select onChange={e => setType(e.currentTarget.value)} id="underline_select" className="m-auto block py-2.5 px-0 w-64 text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer" required>
+                <select onChange={e => setType(encodeURIComponent(e.currentTarget.value))} id="underline_select" className="m-auto block py-2.5 px-0 w-64 text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer" required>
                     <option selected>Choose a hash type</option>
                     <option value="SHA256">SHA-256</option>
                     <option value="SHA512">SHA-512</option>
                     <option value="SHA1">SHA1</option>
                     <option value="MD5">MD5</option>
                 </select>
-                <button className="m-auto block mt-10 bg-green-200 p-2 px-8">Decrypt</button>
-                
-                
+                <button onClick={() => makeRequest()} className="m-auto block mt-10 bg-green-200 p-2 px-8">Decrypt</button>
+                {
+                    loading && (
+                        <div role="status">
+                            <svg aria-hidden="true" className="block m-auto w-8 h-8 mt-10 text-gray-200 animate-spin dark:text-gray-600 fill-pink-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                            </svg>
+                        </div>
+                    )
+                }
+                <p className={messageClass}>{message}</p>
+
 
             </div>
-            <p>{hash}</p>
-            <p>{salt}</p>
-            <p>{type}</p>
         </main>
     )
 }
